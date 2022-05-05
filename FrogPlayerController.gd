d73a023b3cfea63cbdb659e51947c470172ceb15
Kierro DeCampanille <pentawick@gmail.com>
2022-05-05 15:54:05 +0800

    Initial commit

---
+++ FrogPlayerController.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+export var speed := 500
+export var friction := 0.18
+
+var _velocity := Vector2.ZERO
+
+
+func _physics_process(delta):
+	var direction := Vector2(
+		Input.get_action_strength("game_right")-Input.get_action_strength("game_left"),
+		Input.get_action_strength("game_down")-Input.get_action_strength("game_up")
+	)
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+	var target_velocity = direction * speed
+	_velocity += (target_velocity - _velocity) * friction
+	#_velocity = move_and_slide(_velocity)
+	print(_velocity);
